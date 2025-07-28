8f521d6da477bb6cc2d059d42900ffbbf7e5d6ed
ABU ZAHID MD JALAL UDDIN JOY <33056177+azmjalaluddinjoy@users.noreply.github.com>
2025-07-28 00:38:19 -0400

    Creating URLs pattern and Multiple Apps

--- Blogs/views.py
+++ Blogs/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def blog1(request0):
+def blog1(request):
     return HttpResponse('<p> This is our first blog<p>')
--- registration/views.py
+++ registration/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def machine(request):
-    return HttpResponse('Welcome to Machine Django from Joy')
+    return HttpResponse('<h1>Welcome to Machine Django from Joy</h1>')
 
 def deep_learning(request):
     return HttpResponse('We have full Deep Learning course')
